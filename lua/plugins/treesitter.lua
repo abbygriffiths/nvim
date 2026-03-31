2bb4e84de00acc140d0a1b1a7c823052003d8d69
Abirbhav Goswami <abirbhav.goswami@cat.com>
2026-03-31 15:43:15 +0530

    feat: Start Treesitter automatically

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,13 @@
+-- Start Treesitter automatically for the listed file types
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "python", "lua", "bash", "gitignore", "json", "yaml", "shell" },
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
+
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
 }
